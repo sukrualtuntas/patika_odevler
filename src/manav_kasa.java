948e3910dfd0ffcd768bdbbab1236060edb5e99c
Şükrü ALTUNTAŞ <altuntasukru@gmail.com>
2022-11-18 14:50:41 +0300

    manav

--- src/manav_kasa.java
+++ src/manav_kasa.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class manav_kasa {
     public static void main(String[]args){
-        double armut,elma,domates,muz,patlıcan;
-        double armutf =2.14,elmaf=3.67,domatesf=1.11,muzf=0.95,patlıcanf=5.0,toplam=0,tkp;
+        double armut,elma,domates,muz,patlican;
+        double armutf =2.14,elmaf=3.67,domatesf=1.11,muzf=0.95,patlicanf=5.0,toplam=0,tkp;
         Scanner imp = new Scanner(System.in);
         System.out.print("Armut kaç kilo? :");
         armut = imp.nextDouble();
@@ -22,8 +22,8 @@ public class manav_kasa {
         tkp = muz*muzf;
         toplam += tkp;
         System.out.print("Patlıcan kaç kilo?;");
-        patlıcan = imp.nextDouble();
-        tkp = patlıcanf*patlıcan;
+        patlican = imp.nextDouble();
+        tkp = patlicanf*patlican;
         toplam += tkp;
         System.out.print("Toplam tutar:"+toplam+"TL");
 
